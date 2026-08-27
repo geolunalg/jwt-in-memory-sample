e09fea11832b487b018e116cba2e528965f94f3a
geolunalg <geovanni_luna@yahoo.com>
2026-08-27 03:19:41 +0000

    fake server login and refresh

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // server.js
 import jsonServer from "json-server";
 import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const server = jsonServer.create();
 const router = jsonServer.router("./server/db.json");
@@ -10,6 +11,7 @@ const route = "/api/v1"
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+server.use(cookieParser());
 
 server.post(`${route}/login`, (req, res) => {
   const { username, password } = req.body;
@@ -33,11 +35,69 @@ server.post(`${route}/login`, (req, res) => {
     }
   );
 
+  const refreshToken = jwt.sign(
+    {
+      sub: "1",
+      type: "refresh",
+    },
+    "fake-refresh-secret",
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: false, // true when using HTTPS
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
   res.json({
     accessToken: token,
     tokenType: "Bearer",
     expiresIn: 3600,
   });
+});
+
+server.post(`${route}/refresh`, (req, res) => {
+  console.log(req.cookies)
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return res.status(401).json({
+      message: "Refresh token missing",
+    });
+  }
+
+  try {
+    const payload = jwt.verify(
+      refreshToken,
+      "fake-refresh-secret"
+    );
+
+    const accessToken = jwt.sign(
+      {
+        sub: payload.sub,
+        username: "test@example.com",
+        role: "user",
+      },
+      "fake-secret",
+      {
+        expiresIn: "15m",
+      }
+    );
+
+    res.json({
+      accessToken,
+      tokenType: "Bearer",
+      expiresIn: 900,
+    });
+  } catch {
+    return res.status(401).json({
+      message: "Invalid refresh token",
+    });
+  }
 });
 
 function requireAuth(req, res, next) {
